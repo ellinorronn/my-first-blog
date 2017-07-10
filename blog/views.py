9aee20ab27da683ff0e7f749ac115deadc1b271f
Ellinor Rönn <ellinor.ronn@gmail.com>
2017-07-10 17:33:38 +0200

    tried to fix image

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from .forms import PostForm
 def post_list(request):
     # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     posts = Post.objects.order_by('published_date')
+#    posts = Post.objects.order_by('title')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
