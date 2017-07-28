47ddfed78c5e8f912ab4d0e24cc7a6dfc2e19d37
Ellinor Rönn <ellinor.ronn@gmail.com>
2017-07-28 18:00:40 +0200

    Tried to fix index page

--- blog/views.py
+++ blog/views.py
@@ -6,10 +6,10 @@ from .forms import PostForm
 def index(request):
 	return render(request, 'blog/index.html')
 
-def post_list(request):
+#def post_list(request):
     # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    posts = Post.objects.order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+#    posts = Post.objects.order_by('published_date')
+#    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def blog(request):
     # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
